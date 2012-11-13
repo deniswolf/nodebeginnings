f73d4a5dd932a5317cfaf2ac81e2a4f7c507fdaf
DW <dear.mr.dw@gmail.com>
2012-11-14 00:58:12 +0200

    simplified http

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.write("Hello World!");
+  response.end();
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
